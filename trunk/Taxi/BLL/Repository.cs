794358e4a7e9ffc7678120c8d68cb49b54385c99
jangrzegrzolka <jangrzegrzolka@5d5ebe83-0d18-b52f-e0d2-0fb77dbdd8ff>
2011-04-13 23:15:18 +0000

    funkcja do logowania, funkcja zwracajaca wszystkich taksówkarzy, projekt TestProject

--- trunk/Taxi/BLL/Repository.cs
+++ trunk/Taxi/BLL/Repository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DAL;
+using System.Security.Cryptography;
 
 namespace BLL
 {
@@ -24,6 +25,52 @@ namespace BLL
             return x.ToList();
         }
 
+        /// <summary>
+        /// Funkcja autoryzujące użytkownika
+        /// funkcja zwraca userId
+        /// 
+        /// ret -1 => brak uzytkownika o podanym loginie
+        /// ret -2=> zle haslo
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="pswd"></param>
+        /// <returns></returns>
+        public static int UserAuth(string login, string pswd)
+        {
+
+            TaxiDataClassesDataContext ctx = new TaxiDataClassesDataContext();
+
+            var query = from u in ctx.Employees
+                        where u.login == login
+                        select u;
+
+            Employee user = query.SingleOrDefault();
+
+            if (user == null) return -1;
+
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+            String pswdSalt = pswd + user.salt;
+
+            byte[] passwordHash = sha.ComputeHash(ascii.GetBytes(pswdSalt));
+            string password = Convert.ToBase64String(passwordHash);
+
+            if (user.password != password) return -2;
+
+            return user.id;
+        }
+
+        /// <summary>
+        /// Funkcja zwraca listę wszystkich taksówkarz
+        /// </summary>
+        /// <returns></returns>
+        public static List<TaxiDriver> GetAllTaxiDrivers()
+        {
+            TaxiDataClassesDataContext ctx = new TaxiDataClassesDataContext();
+            var query = from c in ctx.Employees.OfType<TaxiDriver>() select c;
+            return query.ToList();
+        }
 
     }
 }
---
+++ trunk/Taxi/TestProject/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using DAL;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethodGetAllTaxiDrivers()
+        {
+            List<TaxiDriver> l = Repository.GetAllTaxiDrivers();
+
+            Assert.IsNotNull( l.First() );
+            Assert.IsTrue(l.First() is TaxiDriver);
+            
+        }
+    }
+}
