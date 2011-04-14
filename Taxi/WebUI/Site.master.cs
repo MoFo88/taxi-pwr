f76be27a9415c714c647473a899c2b2f36c0c3e4
daroc1@gmail.com <daroc1@gmail.com@5d5ebe83-0d18-b52f-e0d2-0fb77dbdd8ff>
2011-04-14 21:53:41 +0000

    Dodanie systemu logowania na stronę

--- Taxi/WebUI/Site.master.cs
+++ Taxi/WebUI/Site.master.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BLL;
+using System.Web.Security;
 
 public partial class SiteMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void LoginControl_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        Login log = (Login)LoginView1.FindControl("Login1");
+        int wynik = Repository.UserAuth(log.UserName, log.Password);
+        //int wynik = Repository.UserAuth("Abc", "Def");
+        if (wynik>=0)
+        {
+            e.Authenticated = true;
+            Session["userName"] = log.UserName;
+        }
+    }
+
+    protected void LoginView1_ViewChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button_logout_Click(object sender, EventArgs e)
+    {
+        Session.Abandon();
+        FormsAuthentication.SignOut();
+        Response.Redirect("Default.aspx");
     }
 }
