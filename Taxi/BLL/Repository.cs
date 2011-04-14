cc969432c801ce93cfd30241fdcb1fa583577c92
jangrzegrzolka <jangrzegrzolka@5d5ebe83-0d18-b52f-e0d2-0fb77dbdd8ff>
2011-04-14 03:04:46 +0000

    drobne zmiany w bazie (pesel = string, adres rozbity na składowe), dodanie autoryzacju uzytwonika, dodawanie taksowkarza po danych, dodawanie taksowkarza po imieniu, nazwisku, loginie i hasle, testy jednostkowe

--- Taxi/BLL/Repository.cs
+++ Taxi/BLL/Repository.cs
@@ -70,7 +70,109 @@ namespace BLL
             TaxiDataClassesDataContext ctx = new TaxiDataClassesDataContext();
             var query = from c in ctx.Employees.OfType<TaxiDriver>() select c;
             return query.ToList();
+        }   
+       
+
+        #region Losowy ciąg znaków o zadanej długości
+        #endregion
+        private static string CreateRandomString(int length)
+        {
+
+            Random random = new Random();
+           
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] hash = md5.ComputeHash(Encoding.ASCII.GetBytes(random.Next().ToString()));
+            string password = Convert.ToBase64String(hash).Substring(0, length);
+            string newPass = "";
+            
+            // Uppercase at random 
+            random = new Random();
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (random.Next(0, 2) == 1)
+                    newPass += password.Substring(i, 1).ToUpper();
+                else
+                    newPass += password.Substring(i, 1);
+            }
+            return newPass;
         }
 
+
+        /// <summary>
+        /// ret -1 => uzytlownik o podanym loginie istnieje
+        /// ret 0 => OK
+        /// ret -2 => błąd przy dodawaniu do bazy
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="city"></param>
+        /// <param name="email"></param>
+        /// <param name="houseNr"></param>
+        /// <param name="pesel"></param>
+        /// <param name="licenceNr"></param>
+        /// <param name="postalCode"></param>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static int AddNewTaxiDriver(String name, String surname, String city, String email, String houseNr, String pesel, String licenceNr, String postalCode, String login, String password)
+        {
+            TaxiDataClassesDataContext ctx = new TaxiDataClassesDataContext();
+            TaxiDriver td = new TaxiDriver();
+
+            td.name = name;
+            td.surname = surname;
+            td.city = city;
+            td.e_mail = email;
+            td.house_nr = houseNr;
+            td.licence_number = licenceNr;
+            td.login = login;
+            td.pesel = pesel;
+            td.postal_code = postalCode;
+
+            Employee check = ctx.Employees.SingleOrDefault(u => u.login == td.login);
+
+            if (check != null)
+            {
+                return -1;
+            }
+
+            td.salt = CreateRandomString(8);
+
+            ASCIIEncoding asci = new ASCIIEncoding();
+            SHA1 sha1 = new SHA1CryptoServiceProvider();    
+
+            String pwd;
+
+            if (password != null)
+            {
+                pwd = Convert.ToBase64String(sha1.ComputeHash( asci.GetBytes(password + td.salt ) ));
+            }
+            else
+            {
+                string randomPwd = CreateRandomString(8);
+                pwd = Convert.ToBase64String(sha1.ComputeHash(asci.GetBytes(randomPwd + td.salt)));
+            }
+
+            td.password = pwd;
+
+            try
+            {
+                ctx.Employees.InsertOnSubmit(td);
+                ctx.SubmitChanges();
+            }
+            catch(Exception ex)
+            {
+                return -2;
+            }
+
+            return 0;
+        }
+
+        public static int AddNewTaxiDriver(String name, String surname, String login, String password)
+        {
+            return AddNewTaxiDriver(name, surname, null, null, null, null, null, null, login, password);
+        }
     }
+
+    
 }
