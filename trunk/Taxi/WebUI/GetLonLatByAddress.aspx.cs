4c646608a5ef87055489d83733742b1b1fa19296
daroc1@gmail.com <daroc1@gmail.com@5d5ebe83-0d18-b52f-e0d2-0fb77dbdd8ff>
2011-05-08 22:11:59 +0000

    Zaznaczanie na mapie punktu podczas wpisywania adresu do okienka dodawania/zmiany zgłoszenia Skrypt korzysta z adresu: http://maps.google.com/maps/geo

---
+++ trunk/Taxi/WebUI/GetLonLatByAddress.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+/*
+ * Ten plik jest potrzebny, ponieważ w jQuery jest problem z zapytaniami cross-domain. Dlatego jQuery odwołuje się do lokalnego pliku, a w C# pobieramy odpowiednie dane ze zdalnego serwera
+ */
+
+public partial class GetLonLatByAddress : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Uri uri = new Uri("http://maps.google.com/maps/geo?output=json&oe=utf-8&q="+Request["address"]+"&key=ABQIAAAAgJQSLzz5oDNMhvRgyTHxWRT3x038EYAEXgYKGEQCIoEp3dSvOxSfVQ_vNywU6Z7AOW5q1YgtkHeH0Q&mapclient=jsapi&hl=pl&callback=_xdc_._3gng7uor0");
+        HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uri);
+        using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+        {
+            using (StreamReader reader = new StreamReader(webResponse.GetResponseStream()))
+            {
+                String response="";
+                response = reader.ReadToEnd();
+                div_resp.InnerHtml = response;
+            }
+        }
+    }
+}
