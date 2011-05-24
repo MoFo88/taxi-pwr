d5e47033c8bbbee37c15053d03483a49a83fb833
Esmersharr <Esmersharr@5d5ebe83-0d18-b52f-e0d2-0fb77dbdd8ff>
2011-05-24 20:59:32 +0000

    Dodawanie kursów

--- trunk/Taxi/WebUI/ChangeOrders.aspx.cs
+++ trunk/Taxi/WebUI/ChangeOrders.aspx.cs
@@ -4,10 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DAL;
+using BLL;
 
 public partial class ChangeOrders : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (this.Page.Form.Method == "post")
+        {
+            String idCourse = this.Page.Request.Form["id_order"];
+            String destination = this.Page.Request.Form["startpoint_name"];
+            String date = this.Page.Request.Form["course_date"];
+            String client = this.Page.Request.Form["client_name"];
+            String clientPhone = this.Page.Request.Form["client_phone"];
+            String lonString = this.Page.Request.Form["lon"];
+            Decimal lon = Decimal.Parse(lonString.Replace(".", ","));
+            String latString = this.Page.Request.Form["lat"];
+            Decimal lat = Decimal.Parse(latString.Replace(".", ","));
+            if (idCourse == "")
+            {
+                addNewCourse(destination, date, client, clientPhone, lon, lat);
+            }
+            else
+            {
+                int id = int.Parse(idCourse);
+                editCourse(id,destination, date, client, clientPhone, lon, lat);
+            }
+        }
+    }
+
+    private void addNewCourse(String destination, String date, String client, String clientPhone, Decimal lon, Decimal lat)
+    {
+        Repository.addNewCourse(33, null, clientPhone, DateTime.Parse(date), null, client, destination, lon, lat, lon, lat);
+    }
+
+    private void editCourse(int? idCourse, String destination, String date, String client, String clientPhone, Decimal lon, Decimal lat)
+    {
+       
     }
 }
