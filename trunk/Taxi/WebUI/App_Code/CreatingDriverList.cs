55db55a539fa4882511ab78e35e3c1a3ecbff79a
daroc1@gmail.com <daroc1@gmail.com@5d5ebe83-0d18-b52f-e0d2-0fb77dbdd8ff>
2011-05-25 22:09:29 +0000

    poprawienie formularza logowania poprawki do tworzenia listy taksówkarzy, żeby w ogóle plik był poprawny -  w tej chwili status niepoprawnie się ustawia (tzn w ogóle się nie generuje, a lon i lat gdy już się wyświetlają to są jakieś z kosmosu (jedną taksówkę mamy w turcji, drugą w nigerii, trzecią na ocenie), przez to mamy w wygenerowanym pliku .txt np. coś w rodzaju: status: , nie ma wartości, dlatego jest błąd JavaScript, dodałem na końcu jedynkę czy tam zero, żeby w ogóle jakaś wartość była poprawiłem delikatnie wygląd formularza do dodawania pracowników

--- trunk/Taxi/WebUI/App_Code/CreatingDriverList.cs
+++ trunk/Taxi/WebUI/App_Code/CreatingDriverList.cs
@@ -26,14 +26,14 @@ public class CreatingDriverList
     private void generateFile(HttpServerUtility server)
     {
         StreamWriter sw = new StreamWriter(server.MapPath("Lists\\GetDriverList.txt"), false);
-        sw.WriteLine("Drivers = new Array();");
+        sw.WriteLine("drivers = new Array();");
         foreach (TaxiDriver driver in drivers)
         {
-            sw.WriteLine("Driver = {");
-            sw.WriteLine("lon: " + driver.position_lon.ToString().Replace(',', '.') + ",");
-            sw.WriteLine("lat: " + driver.position_lat.ToString().Replace(',', '.') + ",");
+            sw.WriteLine("driver = {");
+            sw.WriteLine("lon: " + driver.position_lon.ToString().Replace(',', '.') + "0,");
+            sw.WriteLine("lat: " + driver.position_lat.ToString().Replace(',', '.') + "0,");
             sw.WriteLine("id_driver: " + driver.id + ",");
-            sw.WriteLine("status: " + driver.driver_status_id + ",");
+            sw.WriteLine("status: 1" + driver.driver_status_id + ",");
             sw.WriteLine("registration_number: 'ASD 1234'" + ",");
             sw.WriteLine("license_number: '" + driver.licence_number + "'};");
             sw.WriteLine("drivers[drivers.length] = driver;");
